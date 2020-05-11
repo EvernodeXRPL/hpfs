965aac870cd5b7bb1434393d62ddecf5d88f5910
ravinsp <33562092+ravinsp@users.noreply.github.com>
2020-05-11 23:56:16 +0530

    Fixed write block offset issue.

--- src/fuse_vfs.cpp
+++ src/fuse_vfs.cpp
@@ -149,7 +149,7 @@ int read(const char *vpath, char *buf, size_t size, off_t offset)
     return read_len;
 }
 
-int write(const char *vpath, const char *buf, size_t buf_size, off_t wr_offset)
+int write(const char *vpath, const char *buf, size_t wr_size, off_t wr_start)
 {
     if (hpfs::ctx.run_mode != hpfs::RUN_MODE::RW)
         return -EACCES;
@@ -160,112 +160,67 @@ int write(const char *vpath, const char *buf, size_t buf_size, off_t wr_offset)
     if (!vn)
         return -ENOENT;
 
-    // Find the logical block offset range that should marked for overwrite in the log file.
-    const off_t owr_logical_start_offset = util::get_block_start(MIN(wr_offset, vn->st.st_size));
-    const off_t owr_logical_end_offset = util::get_block_end(wr_offset + buf_size);
-    const size_t owr_size = owr_logical_end_offset - owr_logical_start_offset;
+    if (vfs::update_vnode_mmap(*vn) == -1 || !vn->mmap.ptr)
+        return -1;
 
-    logger::op_write_payload_header wh{owr_size, owr_logical_start_offset, 0};
-    std::vector<uint8_t> owr_vec;
-    iovec owr_buf;
+    const size_t fsize = vn->st.st_size; // Current file size.
+    const size_t wr_end = wr_start + wr_size;
 
-    if (owr_logical_start_offset == wr_offset && owr_size == buf_size)
+    // Find the target file block offset range that should map to memory mapped file.
+    const off_t block_buf_start = util::get_block_start(MIN(wr_start, fsize));
+    const off_t block_buf_end = util::get_block_end(wr_start + wr_size);
+    const size_t block_buf_size = block_buf_end - block_buf_start;
+
+    logger::op_write_payload_header wh{wr_size, wr_start, block_buf_size,
+                                       block_buf_start, (wr_start - block_buf_start)};
+
+    // We maintain list of block buf segments based on where the wr_buf lies within the block buf.
+    std::vector<iovec> block_buf_segs;
+
+    // If write offset is after block start.
+    if (block_buf_start < wr_start)
     {
-        // If the block start/end offsets are in perfect alignment,
-        // log the incoming write buf as it is.
-        owr_buf = {(void *)buf, buf_size};
-    }
-    else
-    {
-        if (vfs::update_vnode_mmap(*vn) == -1 || !vn->mmap.ptr)
-            return -1;
-
-        // Initialize the overwrite block buffer with zeros.
-        owr_vec.resize(owr_size);
-
-        if (wr_offset > owr_logical_start_offset)
+        // If block start offset is before file end, add a segment containing existing
+        // file data between block start and write offset.
+        if (block_buf_start < fsize)
         {
-            // If incoming write lies beyond the overwrite block start, place existing data
-            // on the block buffer.
-            memcpy(owr_vec.data(),
-                   (uint8_t *)vn->mmap.ptr + owr_logical_start_offset,
-                   MIN(wr_offset, vn->st.st_size));
+            const size_t ex_data_len = MIN(fsize, wr_start) - block_buf_start;
+            block_buf_segs.push_back({(uint8_t *)vn->mmap.ptr + block_buf_start, ex_data_len});
         }
 
-        // Place the incoming write buf on the overwrite block buffer.
-        memcpy(owr_vec.data() + (wr_offset - owr_logical_start_offset), buf, buf_size);
+        // If write offset is beyond file size, add a segment for NULL bytes
+        // between file end and write offset.
+        if (fsize < wr_start)
+            block_buf_segs.push_back({NULL, (wr_start - fsize)});
+    }
 
-        owr_buf = {owr_vec.data(), owr_size};
+    // Add segment for write buf.
+    block_buf_segs.push_back({(void *)buf, wr_size});
+
+    // If write end offset is before the block end.
+    if (wr_end < block_buf_end)
+    {
+        // If write end offset is before file end, add a segment containing existing
+        // file data after write end.
+        if (wr_end < fsize)
+            block_buf_segs.push_back({((uint8_t *)vn->mmap.ptr + wr_end), (fsize - wr_end)});
+
+        // If block end is beyond write end offset, add a segment for NULL bytes
+        // between write end and block end.
+        if (wr_end < block_buf_end)
+        {
+            const size_t null_data_len = block_buf_end - MAX(fsize, wr_end);
+            block_buf_segs.push_back({NULL, null_data_len});
+        }
     }
 
     iovec payload{&wh, sizeof(wh)};
-    if (logger::append_log(vpath, logger::FS_OPERATION::WRITE, &payload, &owr_buf) == -1 ||
+    if (logger::append_log(vpath, logger::FS_OPERATION::WRITE, &payload,
+                           block_buf_segs.data(), block_buf_segs.size()) == -1 ||
         vfs::build_vfs() == -1)
         return -1;
 
-    return buf_size;
-}
-
-int write_old(const char *vpath, const char *buf, size_t size, off_t offset)
-{
-    if (hpfs::ctx.run_mode != hpfs::RUN_MODE::RW)
-        return -EACCES;
-
-    vfs::vnode *vn;
-    if (get_vnode(vpath, &vn) == -1)
-        return -1;
-    if (!vn)
-        return -ENOENT;
-
-    logger::op_write_payload_header wh{size, offset, 0};
-
-    // Create block data buf
-    const off_t block_data_start = util::get_block_start(offset);
-    const size_t block_data_end = util::get_block_end(offset + size);
-    const size_t block_data_size = block_data_end - block_data_start;
-
-    iovec block_data_buf;
-    std::vector<uint8_t> vec;
-
-    if (block_data_start == offset && block_data_size == size)
-    {
-        // If the block start/end offsets are in perfect alignment,
-        // log the incoming write buf as it is.
-        block_data_buf = {(void *)buf, size};
-    }
-    else
-    {
-        // Construct new buf with block alignment.
-        vec.resize(block_data_size);
-
-        // Read any existing memory-mapped blocks and place on the new buf.
-        if (vfs::update_vnode_mmap(*vn) == -1 || !vn->mmap.ptr)
-            return -1;
-
-        const vfs::vnode_mmap &mmap = vn->mmap;
-        if (mmap.ptr && mmap.size > block_data_start)
-        {
-            off_t read_len = block_data_size;
-            if ((block_data_start + read_len) > mmap.size)
-                read_len = mmap.size - block_data_start;
-
-            memcpy(vec.data(), (uint8_t *)mmap.ptr + block_data_start, read_len);
-        }
-
-        // Overlay the incoming write buf.
-        wh.buf_offset_in_block_data = offset - block_data_start; // Real data offset within the block buf.
-        memcpy(vec.data() + wh.buf_offset_in_block_data, buf, size);
-
-        // Add the block buf to log record payload.
-        block_data_buf = {vec.data(), block_data_size};
-    }
-
-    iovec payload{&wh, sizeof(wh)};
-    if (logger::append_log(vpath, logger::FS_OPERATION::WRITE, &payload, &block_data_buf) == -1 ||
-        vfs::build_vfs() == -1)
-        return -1;
-
-    return size;
+    return wr_size;
 }
 
 } // namespace fuse_vfs
