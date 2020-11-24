13747285d031f4e15858ac61c8353a8145811206
ravinsp <ravin@Ravin.geveo.local>
2020-11-24 16:44:13 +0530

    Block buf segs fix.

--- src/vfs/virtual_filesystem.cpp
+++ src/vfs/virtual_filesystem.cpp
@@ -437,7 +437,7 @@ namespace hpfs::vfs
         const size_t wr_end = wr_start + wr_size;
 
         // Find the target file block offset range that should map to memory mapped file.
-        block_buf_start = BLOCK_END(MIN(wr_start, fsize));
+        block_buf_start = BLOCK_START(MIN(wr_start, fsize));
         block_buf_end = BLOCK_END(wr_start + wr_size);
         const size_t block_buf_size = block_buf_end - block_buf_start;
 
